fbe013f377c08df3ed0c3240e6a04caa3f2d4b39
Eric LI <li.eric.contact@gmail.com>
2019-01-18 20:18:55 +0100

    Maj de libft:

--- libft/srcs/ft_merge_sort_tab.c
+++ libft/srcs/ft_merge_sort_tab.c
@@ -6,11 +6,13 @@
 /*   By: erli <erli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/14 19:14:18 by erli              #+#    #+#             */
-/*   Updated: 2019/01/16 15:38:40 by erli             ###   ########.fr       */
+/*   Updated: 2019/01/16 12:33:54 by erli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_swap_int(int *a, int *b);
 
 static	void	ft_sort_tab_3(int *tab)
 {
@@ -38,7 +40,7 @@ static	void	ft_sort_tab_3(int *tab)
 	}
 }
 
-static	int		*ft_small_sort(int *tab, int len)
+static	int		*ft_small_sort(int* tab, int len)
 {
 	if (len == 1)
 		return (tab);
@@ -70,7 +72,7 @@ static	int		*ft_merge(int *tab1, int len1, int *tab2, int len2)
 	i1 = 0;
 	i2 = 0;
 	while (i < len1 + len2)
-	{
+	{			
 		if (i1 == len1)
 			tab1[i++] = tab2[i2++];
 		else if ((i2 == len2) || (tab3[i1] <= tab2[i2]))
@@ -88,7 +90,7 @@ static	int		*ft_merge_sort(int *tab, int len)
 	int len1;
 	int len2;
 
-	if (len <= 3)
+	if (len <= 3) 
 		return (ft_small_sort(tab, len));
 	len1 = len / 2;
 	len2 = len - len1;
